bd000a2577a352c86413d128f47a05f200ff4727
jasontjia <christopher.ciayadi2511@gmail.com>
2024-05-19 12:00:28 +0800

    commit 12

--- aplikasi_deteksi/hitung_niv.py
+++ aplikasi_deteksi/hitung_niv.py
@@ -6,9 +6,9 @@ y, sr = librosa.load('D:/De La Salle/SEM 8/Tugas Akhir/aplikasi_deteksi/aplikasi
 
 # Hitung nilai nada dari audio
 pitch, _ = librosa.core.piptrack(y=y, sr=sr)
+pitch_values = pitch[pitch > 0]
 
 # Hitung nilai intonasi
-pitch_values = pitch[pitch > 0]
 median_pitch = np.median(pitch_values)
 pitch_diff = np.diff(pitch_values)
 intonasi = np.mean(np.abs(pitch_diff)) if len(pitch_diff) > 0 else 0
